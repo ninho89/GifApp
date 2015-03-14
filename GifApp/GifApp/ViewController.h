5c16e6a6f721b1ab0c9a0891817daafea53a9218
Filippo Aresu <fili.aresu@gmail.com>
2015-03-14 15:14:25 +0100

    create project with CoreData

---
+++ GifApp/GifApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GifApp
+//
+//  Created by Filippo Aresu on 14/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GifApp/GifApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GifApp
+//
+//  Created by Filippo Aresu on 14/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
