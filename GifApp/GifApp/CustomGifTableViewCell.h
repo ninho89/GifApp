fd6b55b16588c146b20f2f9ad04ad32e90c22d25
Filippo Aresu <fili.aresu@gmail.com>
2015-03-14 15:37:49 +0100

    add CustomGifTableViewCell and IBOutlet image

---
+++ GifApp/GifApp/CustomGifTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  CustomGifTableViewCell.h
+//  GifApp
+//
+//  Created by Filippo Aresu on 14/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomGifTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageGifCustom;
+
+@end
---
+++ GifApp/GifApp/CustomGifTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomGifTableViewCell.m
+//  GifApp
+//
+//  Created by Filippo Aresu on 14/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import "CustomGifTableViewCell.h"
+
+@implementation CustomGifTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
