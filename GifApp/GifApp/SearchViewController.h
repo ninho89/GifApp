4a93835c1b7eb84fa81a3604c3ccc256daa25b84
Filippo Aresu <fili.aresu@gmail.com>
2015-03-14 15:33:54 +0100

    add SearchViewController controller

---
+++ GifApp/GifApp/SearchViewController.h
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.h
+//  GifApp
+//
+//  Created by Filippo Aresu on 14/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
