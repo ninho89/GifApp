7a6b6cba33884287117316ad9fbbd2f81c3eee34
Filippo Aresu <fili.aresu@gmail.com>
2015-03-14 15:34:51 +0100

    add DetailGifViewController controller

---
+++ GifApp/GifApp/DetailGifViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailGifViewController.h
+//  GifApp
+//
+//  Created by Filippo Aresu on 14/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailGifViewController : UIViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
