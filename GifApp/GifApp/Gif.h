d806152934a64863905c975107795a9c20ae6068
Filippo Aresu <fili.aresu@gmail.com>
2015-03-14 15:21:53 +0100

    add Gif class

---
+++ GifApp/GifApp/Gif.h
@@ -0,0 +1,13 @@
+//
+//  Gif.h
+//  GifApp
+//
+//  Created by Filippo Aresu on 14/3/15.
+//  Copyright (c) 2015 Filippo Aresu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Gif : NSObject
+
+@end
