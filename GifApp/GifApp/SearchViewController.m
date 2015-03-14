d3eaa5fa777d3b6eda6cdc2207bae35bd5432184
Filippo Aresu <fili.aresu@gmail.com>
2015-03-14 15:56:36 +0100

    add tableView delegate, UISearchBarDelegate and implementation download gif and parse it

--- GifApp/GifApp/SearchViewController.m
+++ GifApp/GifApp/SearchViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "SearchViewController.h"
+#import "CustomGifTableViewCell.h"
+#import "GiffyApiHelper.h"
+#import "GifParser.h"
+#import "Gif.h"
+#import "DetailGifViewController.h"
 
-@interface SearchViewController ()
+NSString *const kCellSearch = @"CellID";
+
+@interface SearchViewController () <UISearchBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *listGif;
+@property (strong, nonatomic)  UISearchBar *searchGif;
+
 
 @end
 
@@ -16,7 +28,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    self.listGif = [[NSMutableArray array]mutableCopy];
+    [self registerCustomCell];
+    
+    self.searchGif = [[UISearchBar alloc]initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, 44)];
+    [self.view addSubview:self.searchGif];
+    self.searchGif.delegate = self;
+    
+}
+
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
+    [self downloadGiffy:self.searchGif.text];
+    [self.searchGif resignFirstResponder];
+    [self.tableView reloadData];
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +54,55 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void) registerCustomCell{
+    [self.tableView registerNib:[UINib nibWithNibName:@"CustomTableViewCell" bundle:nil]forCellReuseIdentifier:kCellSearch];
 }
-*/
+
+-(void)downloadGiffy:(NSString *)endPoint{
+    GiffyApiHelper *gh = [[GiffyApiHelper alloc]init];
+    [gh gifCompletionWithSearch:endPoint completionBlock:^(NSData *resultData) {
+        GifParser *parser = [[GifParser alloc]init];
+        self.listGif = [parser gifData:resultData];
+        [self.tableView reloadData];
+    }];
+}
+
+#pragma mark - TableView DataSource Methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.listGif.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    CustomGifTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:kCellSearch forIndexPath:indexPath];
+    Gif *gif = self.listGif[indexPath.row];
+    
+//    [cell.imageGifCustom sd_setImageWithURL:[NSURL URLWithString:giffy.giffyURL]  placeholderImage:[UIImage imageNamed:@"first"]];
+//    
+    
+    return cell;
+}
+
+
+#pragma mark - TableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    DetailGifViewController *detailGifViewController = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateViewControllerWithIdentifier:@"detailGifViewController"];
+    
+    detailGifViewController.giffy = self.listGif[indexPath.row];
+    detailGifViewController.managedObjectContext = self.managedObjectContext;
+    [self.navigationController pushViewController:detailGifViewController animated:YES];
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 300;
+}
 
 @end
